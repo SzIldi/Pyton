9838e0948dcb893474a5c2f9c427bf2c255ec164
SzIldi <19trini90@gmail.com>
2018-12-11 20:12:37 +0100

    calculator

---
+++ calculator2.py
@@ -0,0 +1,26 @@
+jegy = 4
+if jegy == 5:
+    print "Gratulalok"
+print "ugyes vagy"
+
+x = int(raw_input("Enter the value for x: "))
+print x
+
+y = int(raw_input("Enter the value for y: "))
+print y
+
+operation = raw_input("Choose math operation (+, -, *, /: ")
+print operation
+
+if operation == "+":
+    print x + y
+elif operation == "-":
+    print x - y
+elif operation == "*":
+    print x * y
+elif operation == "/":
+    print x / y
+else:
+    print "You did not provide the correct math operation."
+
+print "Vege"
---
+++ new2.py
@@ -0,0 +1,7 @@
+x = raw_input("Enter the value for x: ")
+print x
+
+y = raw_input("Enter the value for y: ")
+print y
+
+print x + y
